b51e5acaad2f169e99cd8540fc4added4131bb98
Leonardomarialeoni <leonardomarialeoni@gmail.com>
2024-02-01 17:29:14 +0100

    SAVE

--- Unit1-Week2-Lesson4/assets/js/D4.js
+++ Unit1-Week2-Lesson4/assets/js/D4.js
@@ -2,58 +2,53 @@
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
 
-let l1 = 2;
-let l2 = 3;
-
 function area(l1, l2) {
     return l1 * l2;
 
 }
-console.log(area(l1, l2))
+
+console.log('***ESERCIZIO 1***')
+console.log(area(3, 4))
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
  la loro somma moltiplicata per tre.
 */
-let n1 = 5;
-let n2 = 5;
 
 function crazySum(n1, n2) {
     if (n1 === n2) {
-        return (n1 + n2) * 3
+        return (n1 + n2) * 3;
     }
     else {
-        return n1 + n2
+        return n1 + n2;
     }
 }
 
-console.log(crazySum(n1, n2))
+console.log('***ESERCIZIO 2***')
+console.log(crazySum(3, 4))
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-let n3 = 60;
-let n4 = 19;
-
-function crazyDiff(n3, n4) {
-    if ((n3 - n4) > n4) {
-        return (n3 - n4) * 3
+function crazyDiff(n3) {
+    if ((n3 - 19) > 19) {
+        return Math.abs(n3 - 19) * 3
     }
     else {
-        return n3 - n4
+        return Math.abs(n3 - 19)
     }
 }
-console.log(crazyDiff(n3, n4))
+
+console.log('***ESERCIZIO 3***')
+console.log(crazyDiff(60))
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
-
-let n = 400;
 
 function boundary(n) {
     if (n >= 20 && n <= 100 || n === 400) {
@@ -63,7 +58,9 @@ function boundary(n) {
         return false;
     }
 }
-console.log(boundary(n))
+
+console.log('***ESERCIZIO 4***')
+console.log(boundary(45))
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
