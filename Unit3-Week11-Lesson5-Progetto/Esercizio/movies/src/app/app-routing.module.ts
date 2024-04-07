5f701ab252853091c51cc3d77bb48728e65f1495
yournewgoodnameLeonardomarialeoni <yournew@email.comleonardomarialeoni@gmail.com>
2024-04-07 17:42:52 +0200

    save

---
+++ Unit3-Week11-Lesson5-Progetto/Esercizio/movies/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'login', component: LoginComponent }, 
+  { path: 'profile', component: ProfileComponent }, 
+  { path: 'register', component: RegisterComponent }, 
+  { path: '**', redirectTo: '' } // Rotta di fallback per gestire gli URL non validi, reindirizza alla homepage
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Unit3-Week11-Lesson5-Progetto/Esercizio/movies/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
